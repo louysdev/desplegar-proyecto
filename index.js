5c44fb80b6800426fbf557859d4170d3b0f278a4
Louys <louysgenius@gmail.com>
2022-08-16 19:54:37 -0400

    agregado codigo

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
-const express = require('express');
+const express = require('express')
+const path = require('path')
+const ejs = require('ejs')
 
 const app = express()
 
-app.get('/', (req, res) => res.send("Hello world"))
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
+
+app.get('/api/users', (req, res) => res.json([{name: 'ryan'}, {name: 'juanito'}]))
+
+app.get('/profile', (req, res) => {
+    res.render('profile', {
+        name: 'Juanito',
+        age: 30
+    })
+})
+
+app.use(express.static('public'))
 
 app.listen(process.env.PORT || 3000)
 console.log('Servidor en puerto', process.env.PORT || 3000)
