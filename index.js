57a7306a8800ff1e03a846defff55daccd5fdf6e
Louys <louysgenius@gmail.com>
2022-08-16 20:59:15 -0400

    usuarios añadidos

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const path = require('path')
 const ejs = require('ejs')
 const {connectDB} = require('./db')
+const User = require('./models/users')
 
 connectDB()
 
@@ -10,7 +11,10 @@ const app = express()
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
 
-app.get('/api/users', (req, res) => res.json([{name: 'ryan'}, {name: 'juanito'}]))
+app.get('/api/users', async (req, res) => {
+    const users = await User.find()
+    res.json(users)
+})
 
 app.get('/profile', (req, res) => {
     res.render('profile', {
